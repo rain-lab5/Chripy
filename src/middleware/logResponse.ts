bc2a1a89294500149829b9f0fea3dd485b424504
rain-lab5 <ameermalek831@gmail.com>
2026-08-08 17:48:37 +0300

    Add a middleware to log non-ok responses

---
+++ src/middleware/logResponse.ts
@@ -0,0 +1,19 @@
+import type {Request, Response, NextFunction } from "express";
+
+export type MiddleWare = (req : Request, res : Response, next : NextFunction) => void; 
+
+export function middlewareLogResponses(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  res.on("finish", () => {
+    if (res.statusCode < 200 || res.statusCode >= 300) {
+      console.log(
+        `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`,
+      );
+    }
+  });
+
+  next();
+}
